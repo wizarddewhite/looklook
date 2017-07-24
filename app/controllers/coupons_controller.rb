a0f01235ba26e8897fb0337d7a3249ff83394e88
Wei Yang <richard.weiyang@gmail.com>
2017-07-24 09:35:43 +0800

    coupon/get: user get the coupon by accessing the link
    Not that nice, may think about how to make it more easy to use.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -20,6 +20,28 @@ class CouponsController < ApplicationController
     end
   end
 
+  def show
+    @course = Course.find_by(:id => params[:course_id])
+    @coupon = Coupon.find_by_token(params[:id])
+
+    if current_user.has_joined_course?(@course)
+      flash[:warning] = "You have already joined #{@course.title}"
+    else
+      if @coupon.quantity <= 0
+        flash[:warning] = "No coupon left, be earlier next time"
+        redirect_to course_path(@course)
+        return
+      end
+      # decrease the quantity
+      @coupon.quantity -= 1
+      @coupon.save
+      #join the course
+      current_user.join!(@course)
+      flash[:notice] = "You have joined course #{@course.title}"
+    end
+    redirect_to course_path(@course)
+  end
+
   def destroy
     @course = Course.find_by(:id => params[:course_id])
     @coupon = Coupon.find_by(:id => params[:id])
