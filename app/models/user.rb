bf8a9258625247c5e6aecbbe4dbf6119e90c4ba5
Wei Yang <richard.weiyang@gmail.com>
2017-07-27 16:49:42 +0800

    Enable user account confirmation at registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirm_within => 5.days
   validates :nickname, presence: true
 
   scope :name_alphabetical, -> { order('nickname ASC') }
