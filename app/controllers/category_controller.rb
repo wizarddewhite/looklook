2278ad82685a72dd66cfe29743f8e871df7bbb41
Wei Yang <richard.weiyang@gmail.com>
2017-07-31 23:34:13 +0800

    course category: append an empty to the list
    So that use could select back a whole category.
    
    Leave the first level selected, assume user knows what first level category he
    wants.

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,23 +3,24 @@ class CategoryController < ApplicationController
       @fl_category = FlCategory.find_by(:id => params["select"]["fl_category_id"].to_i)
       session[:fl_category_id] = @fl_category.id
       @sl_categories = SlCategory.where(:fl_category_id => @fl_category.id)
-      if @sl_categories.count != 0
-        @sl_category_id = @sl_categories.first.id
-        @course_categories = CourseCategory.where(:sl_category_id => @sl_category_id)
-      else
-        @sl_category_id = 0
-        @course_categories = []
-      end
+      @sl_categories = [SlCategory.new] + @sl_categories
+      @sl_category_id = 0
       #puts @sl_category_id
     end
 
     def cc_category_select
-      session[:sl_category_id] = params["select"]["sl_category_id"].to_i
+      if params["select"]["sl_category_id"].empty?
+        session[:sl_category_id] = nil
+      else
+        session[:sl_category_id] = params["select"]["sl_category_id"].to_i
+      end
       @fl_category = FlCategory.find_by(:id => session[:fl_category_id])
       @sl_categories = SlCategory.where(:fl_category_id => session[:fl_category_id])
+      @sl_categories = [SlCategory.new] + @sl_categories
       @sl_category = SlCategory.find_by(:id => session[:sl_category_id])
-      @sl_category_id = @sl_category.id
+      @sl_category_id = session[:sl_category_id]
       @course_categories = CourseCategory.where(:sl_category_id => @sl_category_id)
+      @course_categories = [CourseCategory.new] + @course_categories
 
       #puts @sl_category.id
     end
