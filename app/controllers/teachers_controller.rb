ef23ad80b8afdc5d2298f1362d1c7ec87ca73b0c
Wei Yang <richard.weiyang@gmail.com>
2017-08-10 12:02:46 +0800

    teacher/show: display description and courses

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,13 @@ class TeachersController < ApplicationController
   def index
     @teachers = User.where(:is_teacher => true)
   end
+
+  def show
+    @teacher = User.find_by(:id => params[:id])
+    if !@teacher.is_teacher
+      flash[:notice] = "Please check your url"
+      redirect_to teachers_path
+    end
+    @courses = Course.where(:user_id => @teacher.id).where(:is_hidden => false)
+  end
 end
