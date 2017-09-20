3628c422805194a610b92c16c25f583d2abab8ff
Wei Yang <richard.weiyang@gmail.com>
2017-09-20 10:07:12 +0800

    video: position is a must

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
+  validates :position, presence: true
 
   belongs_to :course
   has_many :chapters
